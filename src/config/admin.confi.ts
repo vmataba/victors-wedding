ec59080e947f70ab141aa8697cbde716ce6570ee
victor <vmataba0@gmail.com>
2025-08-06 06:53:25 +0300

    refactor: include missing files

--- src/config/admin.confi.ts
+++
@@ -1,8 +0,0 @@
-import {Admin} from "../models/admin.model";
-
-export const defaultAdmin: Admin = {
-    id: '90009',
-    name: 'Victor Mataba',
-    email: 'admin@victorwedding',
-    password: 'victor-es@2025'
-}
